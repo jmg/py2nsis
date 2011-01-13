95a8810a5f1f6d3ac3366f69e6951da6bac3e0d0
jmg.utn <jmg.utn@99b3fdc1-1f1a-7061-2ac3-6dac4a664b03>
2011-01-13 17:00:07 +0000

    Moved files

--- Generators/PyInstaller.py
+++ Generators/PyInstaller.py
@@ -5,25 +5,33 @@ from BaseGenerator import BaseGenerator
 class PyInstaller(BaseGenerator):
     """
         Class to generate the executable using pyInstaller
+
+        params:
+            - data: an AppData object containing the parameters for Nsis
     """
-    def __init__(self):
+    PYINSTALLER_DIR = "Generators/Libs/pyinstaller-1.5-rc/"
+
+    def __init__(self, data):
         commands = []
-        DIST = os.getcwd() + "/installer"
+        DIST = data.root + "/installer"
 
         #configure pyinstaller
-        #cmd = ["python", "Libs/pyinstaller-1.5-rc/Configure.py"]
-        #commands.append(cmd)
+        cmd = ["python", self.PYINSTALLER_DIR + "Configure.py"]
+        commands.append(cmd)
+
         if os.path.exists(DIST):
             shutil.rmtree(DIST)
         #Run the makespec.yp
-        cmd = ["python", "Libs/pyinstaller-1.5-rc/Makespec.py", "--noconsole", "-w",
+        cmd = ["python", self.PYINSTALLER_DIR + "Makespec.py", "--noconsole", "-w",
                "--out=" + DIST,
-               "/home/jm/DESARROLLO/py2nsis/run.py"]
+               data.main_script]
         commands.append(cmd)
+
         #Run the Build.py
-        cmd = ["python", "Libs/pyinstaller-1.5-rc/Build.py",
+        cmd = ["python", self.PYINSTALLER_DIR + "Build.py",
                 DIST + "/run.spec"]
         commands.append(cmd)
+
         #call the execute in the superclass
         for cmd in commands:
             self.execute(cmd)
